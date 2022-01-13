68781d87afefd7c627c87bd422e8b5150c611750
nguyenngocky <nguyenngocky1995@gmail.com>
2022-01-13 14:09:49 +0700

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,6 @@
+const Footer = {
+    render() {
+        return `<footer class="mt-[20px] max-w-5xl text-center py-4 bg-blue-800 mx-auto text-white text-center uppercase">Nguyễn Ngọc Kỳ</footer>`;
+    },
+};
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+    {
+        id: "2", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+    {
+        id: "3", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+    },
+];
+export default data;
