f4e9e4efc912cb1e3e0a136a40e36a37c3052c32
nguyenngocky <nguyenngocky1995@gmail.com>
2022-01-15 10:05:41 +0700

    update css newDetail

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 const data = [
     {
-        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Buổi lễ tôn vinh “Ong vàng Poly” học kỳ Spring 2018 của Cao đẳng FPT Polytechnic Hà Nội lần này có sự tham gia của thầy Vũ Chí Thành – Giám đốc khối Đào tạo Cao đẳng – FPT Polytechnic, thầy Lã Ngọc Quang – Trưởng ban Đào tạo khối Đào tạo Cao đẳng, anh Lê Quốc Nam – Trưởng phòng Marketting –  FPT Polytechnic",
     },
     {
-        id: "2", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+        id: "2", title: "Sinh viên khối ngành Kinh tế học cách 'va chạm' vào cảm xúc", img: "https://caodang.fpt.edu.vn/wp-content/uploads/tham-quan.jpg", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi những chia sẻ đáng quý từ chính kinh nghiệm bản thân của diễn giả: Trần Hồng Hải – Chủ tịch HĐQT Công ty CP Đào tạo và phát triển Công nghệ truyền thông HAITRAN.",
     },
     {
-        id: "3", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+        id: "3", title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", img: "https://caodang.fpt.edu.vn/wp-content/uploads/IMG_20180608_145330.jpg", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp và tham gia các chương trình talkshow, chia sẻ kinh nghiệm cùng chuyên gia.",
     },
 ];
 export default data;
--- src/pages/newsDetail.js
+++ src/pages/newsDetail.js
@@ -4,10 +4,10 @@ const NewsDetail = {
     render(id) {
         const found = data.find((element) => element.id === id);
 
-        return `<div class="max-w-5xl mx-auto">
-            <h1>${found.title}</h1>
-            <img src="${found.img}" />
-            <p>${found.desc}</p>
+        return `<div class="max-w-5xl mx-auto text-center">
+            <h1 class="font-semibold text-[20px] py-[15px]">${found.title}</h1>
+            <div><img class="w-[506px] m-auto py-[15px]" src="${found.img}" /></div>
+            <p class="text-left">${found.desc}</p>
         </div>`;
     },
 };
